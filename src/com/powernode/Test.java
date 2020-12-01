b0634e7989e60f5182b64dd453c2b53d45854552
2472285583 <2472285583@qq.com>
2020-12-01 11:21:25 +0800

    我的测试项目

---
+++ src/com/powernode/Test.java
@@ -0,0 +1,10 @@
+package com.powernode;
+
+public class Test {
+    public static void main(String[] args) {
+        int i=1;
+        if(i==1){
+            System.out.println("test结束");
+        }
+    }
+}
